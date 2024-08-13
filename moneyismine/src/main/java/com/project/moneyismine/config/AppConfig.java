f501c573bf845b1ba6507d3a09004026f4a3fa36
sunghyun JE <khaos1004@naver.com>
2024-08-13 17:59:22 +0900

    Planning, I think I'll probably develop a pg

---
+++ moneyismine/src/main/java/com/project/moneyismine/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.project.moneyismine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    String apiKey = "REST API Key를 입력";
+    String scretKey = "REST API Secret를 입력";
+
+//    @Bean
+//    public IamportClient iamportClient() {
+//
+//    }
+}
